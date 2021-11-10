8ef2c5c54d5a9a701a45cc6f73d3e07861ca1ca9
Argenis Arriojas <ArriojasMaldonado001@umb.edu>
2021-11-10 08:11:37 -0500

    Include csv_db

---
+++ data/csv_db.py
@@ -0,0 +1,36 @@
+# This db will read from a csv file for each query
+import os
+import pandas as pd
+
+from data import data_path
+
+
+tables_cache = {}
+
+
+def select(species, table, cols=None, offset=None, nrows=None, force_read=False, **kvargs):
+    table_filename = f'{species}_{table}.tsv'
+    table_path = os.path.join(data_path, table_filename)
+
+    if force_read:
+        df = pd.read_csv(table_path, sep='\t', skiprows=offset, nrows=nrows)
+
+    else:
+        if not table_filename in tables_cache.keys():
+            # print(f'+++ start fetching {table_filename} ... reading file ...')
+            tables_cache[table_filename] = pd.read_csv(table_path, sep='\t')
+        else:
+            # print(f'+++ start fetching {table_filename} ... using cache ...')
+            pass
+        
+        df = tables_cache[table_filename].iloc[offset:offset+nrows]
+
+    for column, value in kvargs.items():
+        df = df.loc[df[column] == value]
+
+    if cols is not None:
+        df = df.loc[:, cols]
+
+    # print(f'--- done fetching {table_filename}')
+    return df
+
